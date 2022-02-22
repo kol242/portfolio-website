af76a46ecac13ffa97b8abd911eac8404b8fafd2
kol242 <kolinger315@gmail.com>
2022-02-22 23:11:30 +0100

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -7,7 +7,6 @@ import { Container } from './LayoutStyles'
 export const Layout = ({children}) => {
   return (
     <Container>
-     <Header/>
      <main>{children}</main> 
      <Footer/>
     </Container>
